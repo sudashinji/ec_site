c46932bbac71c747274ed426705af19c908324d2
sudashinji <suda.shinji.infratop@gmail.com>
2020-06-17 11:31:02 +0000

    3_feature

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,9 @@ class Item < ApplicationRecord
     has_many :cart_items
     has_many :order_details
     attachment :image
+    belongs_to :genre
 
-	enum status: {販売中:0, 売り切れ:1}
+	enum status: {"販売中":0, "売り切れ":1}
 
 
 end
