cc2c3a6c13ace4bc4b635f993d2ceeb746982d82
sudashinji <suda.shinji.infratop@gmail.com>
2020-06-24 10:22:33 +0000

    first

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,58 @@ Admin.create!(
              email: "admin@admin",
              password:  "123456",
              password_confirmation: "123456",)
+
+Item.create!(
+
+			name: 'ショートケーキ',
+			description: 'ショートケーキです',
+			genre_id: 'ケーキ',
+			price: '1200',
+			status: '販売中',
+			image: open('./app/assets/images/ショートケーキ.jpg')
+            )
+
+Item.create!(
+	        name: 'チョコレートケーキ',
+			description: 'チョコレートケーキです',
+			genre_id: 'ケーキ',
+			price: '1200',
+			status: '販売中',
+			image: open('./app/assets/images/チョコケーキ.jpg')
+            )
+
+Item.create!(
+	        name: 'モンブラン',
+			description: 'モンブランです',
+			genre_id: 'ケーキ',
+			price: '1000',
+			status: '販売中',
+			image: open('./app/assets/images/モンブラン.jpg')
+            )
+
+Item.create!(
+	        name: 'クッキー',
+	    	description: 'クッキーです',
+			genre_id: 'クッキー',
+			price: '700',
+			status: '販売中',
+			image: open('./app/assets/images/クッキー.jpg')
+			)
+
+Item.create!(
+	        name: 'プリン',
+	    	description: 'プリンです。',
+			genre_id: 'プリンゼリー',
+			price: '600',
+			status: '販売中',
+			image: open('./app/assets/images/プリン.jpg')
+			)
+
+Item.create!(
+	        name: 'イチゴ大福',
+	    	description: '苺大福です',
+	    	genre_id: '和菓子',
+			price: '500',
+			status: '販売中',
+			image: open('./app/assets/images/苺大福.jpg')
+			)
